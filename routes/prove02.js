f9a276958b8c082178aab7843f1766b96a39d515
mueller14003 <kyle.mueller.ghs@gmail.com>
2021-04-30 23:49:44 -0600

    Now with error checking!

--- routes/prove02.js
+++ routes/prove02.js
@@ -12,6 +12,10 @@ let books = [{
     "summary":"Book2 summary goes here."
 }];
 
+let titleError = false;
+let authorError = false;
+let summaryError = false;
+
 router.get('/',(req, res, next) => {
     res.render('pages/prove02', { 
         title: '02 Prove: Assignment', 
@@ -19,6 +23,9 @@ router.get('/',(req, res, next) => {
         activePP02: true, // For HBS
         contentCSS: true, // For HBS
         books: books,
+        titleError: titleError,
+        authorError: authorError,
+        summaryError: summaryError,
     });
 });
 
@@ -26,10 +33,17 @@ router.post('/addBook',(req, res, next) => {
     console.log('Post request!');
     let book = {};
     book.title = req.body.title;
+    titleError = book.title === "";
     book.author = req.body.author;
+    authorError = book.author === "";
     book.summary = req.body.summary;
-    console.log(book);
-    books.push(book);
+    summaryError = book.summary === "";
+    if (titleError || authorError || summaryError) {
+      console.log(`ERROR: Please enter the requested information in all fields.`)
+    } else {
+      console.log(book);
+      books.push(book);
+    }
     res.redirect('/prove02');
 });
 
